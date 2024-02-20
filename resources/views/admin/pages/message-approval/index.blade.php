07e1902e468f9d5d78e7a4982a505e2274136c23
nesaathussholeha <nesaathussholeha@gmail.com>
2024-02-20 15:21:11 +0700

    up approval pesan

--- resources/views/admin/pages/message-approval/index.blade.php
+++ resources/views/admin/pages/message-approval/index.blade.php
@@ -1,99 +1,124 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <div class="table-responsive custom-scrollbar">
-                <div id="basic-1_wrapper" class="dataTables_wrapper no-footer">
-                    <div class="dataTables_length" id="basic-1_length"><label>Show <select name="basic-1_length"
-                                aria-controls="basic-1" class="">
-                                <option value="10">10</option>
-                                <option value="25">25</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                            </select> entries</label></div>
-                    <div id="basic-1_filter" class="dataTables_filter"><label>Search:<input type="search" class=""
-                                placeholder="" aria-controls="basic-1"></label></div>
-                    <table class="display dataTable no-footer " id="basic-1" role="grid"
-                        aria-describedby="basic-1_info">
-                        <thead>
-                            <tr role="row" style="background-color: #36405E; color: white">
-                                <th class="sorting_asc" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-sort="ascending" aria-label="Name: activate to sort column descending"
-                                    style="width: 161.3px;">Name</th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Position: activate to sort column ascending" style="width: 259.225px;">
-                                    Position</th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Office: activate to sort column ascending" style="width: 128.012px;">Office
-                                </th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Age: activate to sort column ascending" style="width: 57.1125px;">Age</th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Start date: activate to sort column ascending" style="width: 116.1px;">Start
-                                    date</th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Salary: activate to sort column ascending" style="width: 81.325px;">Salary
-                                </th>
-                                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
-                                    aria-label="Action: activate to sort column ascending" style="width: 83.525px;">Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr role="row" class="odd">
-                                <td class="sorting_1">Airi Satou</td>
-                                <td>Accountant</td>
-                                <td>Tokyo</td>
-                                <td>33</td>
-                                <td>2008/11/28</td>
-                                <td>$162,700</td>
-                                <td>
-                                    <div class="d-flex gap-2 align-items-center">
-                                        <button class="btn btn-success " type="button">Terima</button>
-                                        <button class="btn btn-danger" type="button">Tolak</button>
-                                        <button style="width: 20px; border-radius:5px" type="button" data-bs-toggle="modal"
-                                            data-bs-target="#edit" class=" btn-info"><i class="fa fa-eye"></i></button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade modal-bookmark" id="edit" tabindex="-1" role="dialog"
-            aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Detail</h5>
-                        <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form class="form-bookmark needs-validation" action="#" method="POST" id="bookmark-form"
-                        novalidate="" enctype="multipart/form-data">
-                        <div class="modal-body">
-                            <div class=" max-w-7xl mx-auto">
-                                <label for="" class="fs-5">Nama</label>
-                                <h5>Airi Satou</h5>
-                                <label for="" class="fs-5">Email</label>
-                                <h5>hY4pW@example.com</h5>
-                                <label for="" class="fs-5">Pesan</label>
-                                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dolore nam temporibus rem
-                                    voluptas laudantium quia facilis consequatur architecto tempora magnam ducimus non
-                                    molestiae quod est repudiandae, assumenda maiores ullam.</h5>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <div class="d-flex justify-content-end">
-                                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancel</button>
-                                <button class="btn btn-primary" type="submit">Simpan</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="row">
+    <div class="col-sm-12 pt-3">
+        <div class="pb-0 d-flex justify-content-between">
+            <h3>Pengajuan</h3>
+            <div class="d-flex align-items-center gap-2">
+                <p class="m-0 me-2">Cari:</p>
+                <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search">
             </div>
         </div>
     </div>
+    <div class="col-sm-12 mt-3">
+        <div class="table">
+            <table class="table table-striped">
+                <thead style="background-color:#36405E;">
+                    <tr>
+                        <th scope="col" style="color: white">No</th>
+                        <th scope="col" style="color: white">Name</th>
+                        <th scope="col" style="color: white">Email</th>
+                        <th scope="col" style="color: white">Pesan</th>
+                        <th scope="col" style="color: white">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>AGDTS</td>
+                            <td>agdts@gmail.com</td>
+                            <td>{{ Str::limit('Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit tenetur veritatis',50)}}</td>
+                            <td class="gap-2 align-items-center" >
+                                <a href="#" class="btn btn-outline-success">Terima</a>
+                                <a href="#" class="btn btn-danger">Tolak</a>
+                                <button type="button" data-bs-toggle="modal" data-bs-target="#edit" class="btn btn-primary px-3"><i class="fa fa-eye"></i></button>
+                            </td>
+                        </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>SGMAN</td>
+                        <td>sgman@gmail.com</td>
+                        <td>{{ Str::limit('Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit tenetur veritatis',50)}}</td>
+                        <td class="gap-2 align-items-center">
+                            <a href="#" class="btn btn-outline-success">Terima</a>
+                            <a href="#" class="btn btn-danger">Tolak</a>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#edit" class="btn btn-primary px-3"><i class="fa fa-eye"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>SPMP</td>
+                        <td>spmp@gmail.com</td>
+                        <td>{{ Str::limit('Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit tenetur veritatis',50)}}</td>
+                        <td class="gap-2 align-items-center" >
+                            <a href="#" class="btn btn-outline-success">Terima</a>
+                            <a href="#" class="btn btn-danger">Tolak</a>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#edit" class="btn btn-primary px-3"><i class="fa fa-eye"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="modal fade modal-bookmark" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Mitra</h5>
+                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form class="form-bookmark needs-validation" action="#" method="POST" id="bookmark-form" novalidate=""
+                    enctype="multipart/form-data">
+                    <div class="modal-body">
+                        <div class="row g-2">
+                            <div class="mb-3 mt-0 col-md-12">
+                                <label for="bm-title">Nama Mitra</label>
+                                <input class="form-control" type="text" required="" autocomplete="name">
+                            </div>
+                            <div class="mb-3 mt-0 col-md-12">
+                                <label>Kategori</label>
+                                <select class="js-example-basic-single" aria-label=".form-select example">
+                                    <option selected="">What's Your Hobbies </option>
+                                    <option value="1">Kho-kho</option>
+                                    <option value="2">Reading Books</option>
+                                    <option value="3">Creativity</option>
+                                </select>
+                            </div>
+                            <div class="mb-3 mt-0 col-md-12">
+                                <label for="bm-title">Foto</label>
+                                <input class="form-control" id="formFile" type="file">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="d-flex justify-content-end">
+                            <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancel</button>
+                            <button class="btn btn-primary" type="submit">Perbarui</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <nav class="m-b-30 mt-3" aria-label="Page navigation example">
+        <ul class="pagination justify-content-center pagin-border-primary pagination-primary">
+            <li class="page-item"><a class="page-link" href="javascript:void(0)">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="javascript:void(0)">1</a></li>
+            <li class="page-item"><a class="page-link" href="javascript:void(0)">2</a></li>
+            <li class="page-item"><a class="page-link" href="javascript:void(0)">3</a></li>
+            <li class="page-item"><a class="page-link" href="javascript:void(0)">Next</a></li>
+        </ul>
+    </nav>
+@endsection
+@section('script')
+    <script>
+        // In your Javascript (external .js resource or <script> tag)
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+        });
+    </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -113,3 +113,7 @@ Route::get('setting/profile', function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
