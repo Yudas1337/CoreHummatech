6260c6a20d1e5b637fd0cb5f38243b84073a90d2
nesaathussholeha <nesaathussholeha@gmail.com>
2024-02-23 22:28:04 +0700

    create social media

--- routes/nesa.php
+++ routes/nesa.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('social-media' , [SosialMediaController::class ,'index']);
 Route::post('create/social/media/' , [SosialMediaController::class , 'store'])->name('create.social.media');
+Route::delete('delete/social/media/{sosialMedia}' ,[SosialMediaController::class ,'destroy'])->name('delete.social.media');
+
 
 //social media
 // Route::get('/social-media' , [SosialMediaController::class ,'index']);
