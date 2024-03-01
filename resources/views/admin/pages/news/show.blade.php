2a0bb952837c5c4d3c36396b130b33853db3a147
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-01 08:54:44 +0700

    detail

--- resources/views/admin/pages/news/show.blade.php
+++ resources/views/admin/pages/news/show.blade.php
@@ -2,21 +2,31 @@
 
 @section('subcontent')
     <div class="page-title">
-            {{ $news->updated_at->format('l, j F Y') }}</p>
+        <div class="d-flex justify-content-between">
+            <p>{{ $news->updated_at->format('l, j F Y') }}</p>
+            <a href="/news" class="btn btn-primary">Kembali</a>
+        </div>
         <h2>{{ $news->title }}</h2>
     </div>
 @endsection
 @section('content')
     <div class="col">
         <div class="img-news">
-            <img src="{{ Storage::url($news->image) }}" class="w-100" alt="{{ $news->title }}" />
+            <img src="{{ asset('storage/'.$news->newsImages[0]->photo ?? '') }}" class="w-100" alt="{{ $news->newsImages[0]->photo ?? '' }}" />
         </div>
-        <div class="news-description mt-3">
+        <div class="news-description">
             {!! $news->description !!}
+        </div>
+        <div class="row">
+            @foreach ($newsImages as $newsImage)
+            <div class="col-12 col-xl-5">
+                <img src="{{ asset('storage/' . $newsImage->photo) }}" alt="">
+            </div>
+            @endforeach
         </div>
         <div class="tags d-flex flex-wrap gap-2">
             <p class="fw-bold pe-2">Tags:</p>
-            @foreach (explode(',', $news->tags) as $index => $tag)
+            @foreach ($categories as $index=>$categorie)
                 @php
                     $colors = ['primary', 'warning', 'danger', 'info', 'success'];
                     $colorIndex = $index % count($colors);
@@ -25,7 +35,7 @@
                 <div class="d-flex">
                     <span class="p-1 bg-{{ $colorClass }}"
                         style="height: 33px; border-radius: 10px; margin-top: -2px"></span>
-                    <p style="background-color:rgba(var(--bs-{{ $colorClass }}-rgb), .125); padding: 3px 10px; border-radius:5px">Keseruan</p>
+                    <p style="background-color:rgba(var(--bs-{{ $colorClass }}-rgb), .125); padding: 3px 10px; border-radius:5px">Tim</p>
                 </div>
             @endforeach
 
