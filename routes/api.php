eb618171efdcb6bc3603050709d17ba6dd055994
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-03-13 09:23:09 +0700

    api testimonial

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TestimonialController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::get('testimonial', [TestimonialController::class, 'index']);
+    Route::post('testimonial', [TestimonialController::class, 'store']);
+    Route::post('testimonial-product', [TestimonialController::class, 'storeProduct']);
+    Route::put('testimonial/{testimonial}', [TestimonialController::class, 'update']);
+    Route::put('testimonial-product/{testimonial}', [TestimonialController::class, 'updateProduct']);
+    Route::delete('testimonial', [TestimonialController::class, 'destroy']);
 });
