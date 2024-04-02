d8169ec49041db4d8a717840971f564de06c3f3e
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-04-02 19:48:51 +0700

    Update profile.blade.php

--- resources/views/landing/about/profile.blade.php
+++ resources/views/landing/about/profile.blade.php
@@ -1,5 +1,5 @@
 @extends('landing.layouts.layouts.app')
-@section('title' , 'About Us Company')
+@section('title' , 'About Company')
 @section('style')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
 
