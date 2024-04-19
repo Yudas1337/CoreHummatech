a9eceb3fc9ddb97bb2b9c6c3ed6bd6ccaf75e5d2
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-04-19 14:34:31 +0700

    re-slicing news landingpage

--- resources/views/landing/news/index.blade.php
+++ resources/views/landing/news/index.blade.php
@@ -48,7 +48,7 @@
         </div>
     </div>
 
-    <div class="about-us-area">
+    {{-- <div class="about-us-area">
         <div class="container">
             <ul class="nav custom-tabs">
                 <li @if (!request()->category) class="active" @endif><a href="{{ url('/news') }}">Terbaru</a></li>
@@ -59,96 +59,121 @@
                 @endforeach
             </ul>
         </div>
-    </div>
+    </div> --}}
 
-    <div class="blog-area full-blog blog-standard full-blog grid-colum mt-5 mb-5">
+    <div class="blog-area right-sidebar full-blog mt-5">
         <div class="container">
-            <div class="blog-items content-less">
-                <div class="blog-content">
-                    <div class="blog-item-box">
-                        <div class="row">
-                            @forelse ($newses as $news)
-                                @if ($news->news)
-                                    <div class="col-lg-4 col-md-6 single-item">
-                                        <div class="item">
-                                            <div class="thumb">
-                                                <a href="/news/{{ $news->news->slug }}">
-                                                    <img
-                                                        src="{{ asset('storage/' . $news->news->thumbnail) }}"
-                                                        alt="{{ $news->news->title }}">
-                                                </a>
-
-                                                <time class="date"
-                                                    datetime="">{{ \Carbon\Carbon::parse($news->news->date)->locale('id_ID')->isoFormat('D MMMM Y') }}</time>
-                                            </div>
-                                            <div class="info">
-                                                <div class="meta">
-                                                    <ul>
-                                                        <li>
-                                                            <img src="{{ asset('mobilelogo.png') }}"
-                                                                alt="Hummatech Logo" />
-                                                            <span>By </span>
-                                                            <a href="javascript:void(0)">Hummatech</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-
-                                                <h4>
-                                                    <a href="/news/{{ $news->news->slug }}">{{ $news->news->title }}</a>
-                                                </h4>
-
-                                                <p class="">{!! Str::limit(strip_tags($news->news->description), 200) !!}</p>
-                                            </div>
-                                        </div>
+            <div class="blog-items">
+                <div class="row">
+                    <div class="col-12 col-xl-3">
+                        <!-- Start Sidebar -->
+                        <div class="sidebar wow fadeInLeft card item border-0 py-4">
+                            <aside>
+                                <div class="sidebar-item recent-post">
+                                    <div class="title">
+                                        <h4>Kategori berita</h4>
                                     </div>
-                                @else
-                                <div class="col-lg-4 col-md-6 single-item">
-                                        <div class="item">
-                                            <div class="thumb">
-                                                <a href="/news/{{ $news->slug }}"><img
-                                                        src="{{ asset('storage/' . $news->thumbnail) }}"
-                                                        alt="{{ $news->title }}"></a>
-
-                                                <time class="date"
-                                                    datetime="">{{ \Carbon\Carbon::parse($news->date)->locale('id_ID')->isoFormat('D MMMM Y') }}</time>
-                                            </div>
-                                            <div class="info">
-                                                <div class="meta">
-                                                    <ul>
-                                                        <li>
-                                                            <img src="{{ asset('mobilelogo.png') }}"
-                                                                alt="Hummatech Logo" />
-                                                            <span>By </span>
-                                                            <a href="javascript:void(0)">Hummatech</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-
-                                                <h4>
-                                                    <a href="/news/{{ $news->slug }}">{{ $news->title }}</a>
-                                                </h4>
-                                                <p class="">{!! Str::limit(strip_tags($news->description), 200) !!}</p>
-                                                {{-- <p class="text-break justify-content-center">{!! Str::limit(strip_tags($news->description), 200) !!}</p> --}}
-                                            </div>
-                                        </div>
+                                    <div class="sidebar-info">
+                                        <ul>
+                                            @foreach ($newsCategories as $category)
+                                                <li>
+                                                    <a href="{{ url("/news/category/{$category->slug}") }}">{{ $category->slug }}</a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
                                     </div>
-                                @endif
-                            @empty
-                                <div class="d-flex justify-content-center col-12 ">
-                                    <img src="{{ asset('nodata-gif.gif') }}" width="600px" alt="" srcset="">
                                 </div>
-                                <h4 class="fs-1 text-center text-dark col-12 " style="font-weight: 600">
-                                    Data Masih Kosong
-                                </h4>
-                            @endforelse
+                            </aside>
                         </div>
-
-                        <!-- Pagination -->
-                        <div class="row">
-                            <div class="col-md-12 pagi-area text-center">
-                                <nav aria-label="navigation">
-                                </nav>
+                        <!-- End Start Sidebar -->
+                    </div>
+                    <div class="col-12 col-xl-9">
+                        <div class="row px-5">
+                            <div class="blog-content col-lg-8 col-12 col-xl-12 col-md-12">
+                                <div class="blog-item-box">
+                                    <div class="single-item">
+                                        <div class="">
+                                            <div class="row">
+                                                @forelse ($newses as $news)
+                                                    @if ($news->news)
+                                                        <div class="col-lg-6 col-md-6 single-item">
+                                                            <div class="item">
+                                                                <div class="thumb">
+                                                                    <a href="/news/{{ $news->news->slug }}">
+                                                                        <img
+                                                                            src="{{ asset('storage/' . $news->news->thumbnail) }}"
+                                                                            alt="{{ $news->news->title }}" class="img-fluid" style="width: 500px; height: 200px; object-fit: cover;">
+                                                                    </a>
+                    
+                                                                    <time class="date"
+                                                                        datetime="">{{ \Carbon\Carbon::parse($news->news->date)->locale('id_ID')->isoFormat('D MMMM Y') }}</time>
+                                                                </div>
+                                                                <div class="info">
+                                                                    <div class="meta">
+                                                                        <ul>
+                                                                            <li>
+                                                                                <img src="{{ asset('mobilelogo.png') }}"
+                                                                                    alt="Hummatech Logo" />
+                                                                                <span>By </span>
+                                                                                <a href="javascript:void(0)">Hummatech</a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </div>
+                    
+                                                                    <h4>
+                                                                        <a href="/news/{{ $news->news->slug }}">{{ $news->news->title }}</a>
+                                                                    </h4>
+                    
+                                                                    <p class="">{!! Str::limit(strip_tags($news->news->description), 200) !!}</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    @else
+                                                    <div class="col-lg-6 col-md-6 single-item">
+                                                            <div class="item">
+                                                                <div class="thumb">
+                                                                    <a href="/news/{{ $news->slug }}"><img
+                                                                            src="{{ asset('storage/' . $news->thumbnail) }}"
+                                                                            alt="{{ $news->title }}" class="img-fluid" style="width: 500px; height: 200px; object-fit: cover;"></a>
+                    
+                                                                    <time class="date"
+                                                                        datetime="">{{ \Carbon\Carbon::parse($news->date)->locale('id_ID')->isoFormat('D MMMM Y') }}</time>
+                                                                </div>
+                                                                <div class="info">
+                                                                    <div class="meta">
+                                                                        <ul>
+                                                                            <li>
+                                                                                <img src="{{ asset('mobilelogo.png') }}"
+                                                                                    alt="Hummatech Logo" />
+                                                                                <span>By </span>
+                                                                                <a href="javascript:void(0)">Hummatech</a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </div>
+                    
+                                                                    <h4>
+                                                                        <a href="/news/{{ $news->slug }}">{{ $news->title }}</a>
+                                                                    </h4>
+                                                                    <p class="">{!! Str::limit(strip_tags($news->description), 200) !!}</p>
+                                                                    {{-- <p class="text-break justify-content-center">{!! Str::limit(strip_tags($news->description), 200) !!}</p> --}}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    @endif
+                                                @empty
+                                                    <div class="d-flex justify-content-center col-12 ">
+                                                        <img src="{{ asset('nodata-gif.gif') }}" width="600px" alt="" srcset="">
+                                                    </div>
+                                                    <h4 class="fs-1 text-center text-dark col-12 " style="font-weight: 600">
+                                                        Data Masih Kosong
+                                                    </h4>
+                                                @endforelse
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
