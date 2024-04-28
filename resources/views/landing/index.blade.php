5879dc48f68f3a11c44112a91cd94cc5393dadd3
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-04-28 20:09:29 +0700

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -436,7 +436,7 @@
             }
 
             .single-item {
-                flex: 1 0 250px; /* Atur lebar kartu sesuai kebutuhan */
+                flex: 1 0 300px; /* Atur lebar kartu sesuai kebutuhan */
                 margin: 10px; /* Atur margin sesuai kebutuhan */
             }
         </style>
