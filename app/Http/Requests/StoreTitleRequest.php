2a0ff4b01a9f3e82559f3c72bcac8893bc9df489
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-04-30 09:11:30 +0700

    crud  title

--- app/Http/Requests/StoreTitleRequest.php
+++ app/Http/Requests/StoreTitleRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreShopRequest extends FormRequest
+class StoreTitleRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,10 +22,7 @@ class StoreShopRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required',
-            'description' => 'required',
-            'image' => 'required',
-            'price' => 'required'
+            'name' => 'required'
         ];
     }
 }
--- app/Http/Requests/UpdateShopRequest.php
+++ app/Http/Requests/UpdateShopRequest.php
@@ -11,7 +11,7 @@ class UpdateShopRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateShopRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+            'price' => 'required'
         ];
     }
 }
