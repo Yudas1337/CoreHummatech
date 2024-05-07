b4878ec850dad1fe445dc8c42a85da539591e473
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-07 10:29:51 +0700

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -113,7 +113,7 @@
 
 @section('content')
     <!-- Start Banner
-                                            ============================================= -->
+                                                ============================================= -->
     <div class="banner-area text-center text-big top-pad-50">
         <div id="bootcarousel" class="carousel text-light slide animate_text" data-ride="carousel">
 
@@ -238,7 +238,7 @@
     <!-- End About -->
 
     <!-- Star Services Area
-                                        ============================================= -->
+                                            ============================================= -->
     <div class="thumb-services-area inc-thumbnail default-padding bottom-less mt-5">
         <div class="right-shape">
             <img src="assets-home/img/shape/9.png" alt="Shape">
@@ -565,19 +565,30 @@
                                                             <div class="item" style="width: 300px">
                                                                 <div class="" style="background-color: #E5F1FF;">
                                                                     <img src="{{ asset('storage/' . $portfolio->image) }}"
-                                                                        class="object-fit-cover" style="object-fit: cover" alt="{{ $portfolio->name }}">
+                                                                        class="object-fit-cover" style="object-fit: cover"
+                                                                        alt="{{ $portfolio->name }}">
                                                                 </div>
                                                                 <div class="content">
                                                                     <div class="info">
                                                                         <h4><a
-                                                                                href="{{ route('detail.product', $portfolio->slug) }}">{{ Str::limit($portfolio->name , 25 ,'...') }}</a>
+                                                                                href="{{ route('detail.product', $portfolio->slug) }}">{{ Str::limit($portfolio->name, 25, '...') }}</a>
                                                                         </h4>
                                                                         <span>{{ Str::limit($portfolio->description, 40, '...') }}</span>
                                                                     </div>
                                                                     <div class="button">
                                                                         <a href="{{ asset('storage/' . $portfolio->image) }}"
                                                                             class="item popup-gallery">
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M11 6H9v3H6v2h3v3h2v-3h3V9h-3z"></path><path d="M10 2c-4.411 0-8 3.589-8 8s3.589 8 8 8a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8zm0 14c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"></path></svg>                                                                        </a>
+                                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                width="27" height="27"
+                                                                                viewBox="0 0 24 24"
+                                                                                style="fill: rgb(255, 255, 255);transform: ;msFilter:;">
+                                                                                <path d="M11 6H9v3H6v2h3v3h2v-3h3V9h-3z">
+                                                                                </path>
+                                                                                <path
+                                                                                    d="M10 2c-4.411 0-8 3.589-8 8s3.589 8 8 8a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8zm0 14c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z">
+                                                                                </path>
+                                                                            </svg>
+                                                                        </a>
                                                                     </div>
                                                                 </div>
                                                             </div>
