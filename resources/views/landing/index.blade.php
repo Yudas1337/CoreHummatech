72c2552b7a63d74c574296672be24443d6830440
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-07 09:55:56 +0700

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -566,19 +566,19 @@
                                                                 <div class="" style="background-color: #E5F1FF;">
                                                                     <img src="{{ asset('storage/' . $portfolio->image) }}"
                                                                         width="300px" height="300px"
-                                                                        class="object-fit-cover" alt="Thumb">
+                                                                        class="object-fit-cover" style="object-fit: cover" alt="Thumb">
                                                                 </div>
                                                                 <div class="content">
                                                                     <div class="info">
                                                                         <h4><a
-                                                                                href="{{ route('detail.product', $portfolio->slug) }}">{{ $portfolio->name }}</a>
+                                                                                href="{{ route('detail.product', $portfolio->slug) }}">{{ Str::limit($portfolio->name , 15 ,'...') }}</a>
                                                                         </h4>
                                                                         <span>{{ Str::limit($portfolio->description, 50, '...') }}</span>
                                                                     </div>
                                                                     <div class="button">
                                                                         <a href="{{ asset('storage/' . $portfolio->image) }}"
                                                                             class="item popup-gallery">
-                                                                            <i class="fas fa-plus"></i>
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5q0-1.875-1.312-3.187T9.5 5Q7.625 5 6.313 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14"/></svg>
                                                                         </a>
                                                                     </div>
                                                                 </div>
