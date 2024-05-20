b4ec5755d1b8ab489129a6456c16129c6a09b1cf
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-05-20 08:22:40 +0700

    Update StoreServiceRequest.php

--- app/Http/Requests/StoreServiceRequest.php
+++ app/Http/Requests/StoreServiceRequest.php
@@ -24,7 +24,7 @@ class StoreServiceRequest extends FormRequest
         return [
             'image' => 'required|mimes:png,jpg,jpeg',
             'name' => 'required|max:255',
-            'slug' => 'required|max:255',
+            'slug' => 'required|max:255|unique:services,slug,except,id',
             'description' => 'required|max:10000',
             'short_description' => 'nullable|min:10|max:100',
             'link' => 'nullable|max:255',
@@ -41,6 +41,7 @@ class StoreServiceRequest extends FormRequest
             'name.max' => 'Nama maksimal :max karakter',
             'slug.required' => 'Slug harus diisi',
             'slug.max' => 'Slug maksimal :max karakter',
+            'slug.unique' => 'Slug sudah digunakan',
             'short_description.min' => 'Deskripsi singkat maksimal :min karakter',
             'short_description.max' => 'Deskripsi singkat maksimal :max karakter',
             'description.required' => 'Deskripsi harus diisi',
